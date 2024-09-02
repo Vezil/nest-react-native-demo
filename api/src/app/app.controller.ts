9facaabc520cdedb9fc7420d714d14866bb73829
Vezil <szymonwojaczek@wp.pl>
2024-09-02 16:07:48 +0200

    add post controlers

--- api/src/app/app.controller.ts
+++ api/src/app/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Post, Body } from '@nestjs/common';
 
 import { AppService } from './app.service';
 
@@ -9,5 +9,15 @@ export class AppController {
     @Get()
     getData() {
         return this.appService.getData();
+    }
+
+    @Post()
+    create(@Body() { text }: { text: string }) {
+        return this.appService.add(text);
+    }
+
+    @Post('setTodoStatus')
+    setDone(@Body() { id, isDone }: { id: number; isDone: boolean }) {
+        return this.appService.setTodoStatus(id, isDone);
     }
 }
